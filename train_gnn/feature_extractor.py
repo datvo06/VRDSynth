17673d2d1f25c97393bfe3eb6c7c613b969bad41
datvo06 <datvo06@gmail.com>
2023-10-01 23:48:20 +1100

    [FEAT]: Adding relation construction using a customized KMeans

--- train_gnn/feature_extractor.py
+++ train_gnn/feature_extractor.py
@@ -4,6 +4,12 @@ from typing import List, Tuple, Dict
 import numpy as np
 import cv2
 from collections import namedtuple
+import os
+from scipy.spatial.distance import cdist
+from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin
+
 
 BoxRel = namedtuple('BoxRel', [i, j, mag, projs])
 
@@ -11,9 +17,47 @@ BoxRel = namedtuple('BoxRel', [i, j, mag, projs])
 EDGE_COLOR_SET = ['red', 'green', 'blue', 'black', 'gray']
 
 
-def dummy_calculate_relation_set(dataset: Dataset, k: int):
+def dummy_calculate_relation_set(dataset: Dataset, k: int, clusters: int):
     """Default: return top, down, left, right"""
     return [(0, 1), (0, -1), (-1, 0), (1, 0)]
+
+
+
+def angle_dist(a, b):
+    # define a function to calculate the distance between angles
+    return np.arccos(np.cos(a - b))
+
+class AngleKMeans:
+    def __init__(self, n_clusters, max_iter=100, random_state=None):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+        self.random_state = random_state
+        self.centers_ = None
+
+    def fit(self, X):
+        rng = np.random.default_rng(self.random_state)
+        
+        # Randomly choose clusters
+        i = rng.permutation(X.shape[0])[:self.n_clusters]
+        self.centers_ = X[i]
+
+        for _ in range(self.max_iter):
+            # Assign labels based on closest center
+            labels = pairwise_distances_argmin(X, self.centers_, metric=angle_dist)
+
+            # Find new centers from means of points
+            new_centers = np.array([X[labels == i].mean(0) for i in range(self.n_clusters)])
+
+            # Check for convergence
+            if np.all(self.centers_ == new_centers):
+                break
+
+            self.centers_ = new_centers
+
+        return self
+
+    def predict(self, X):
+        return pairwise_distances_argmin(X, self.centers_, metric=angle_dist)
 
 
 def calculate_relation_set(dataset: Dataset, k: int, clusters: int) -> List[Tuple[float, float]]:
@@ -21,7 +65,45 @@ def calculate_relation_set(dataset: Dataset, k: int, clusters: int) -> List[Tupl
     # First, use knn to find the k nearest neighbors
     knn_rels = []
     for data in dataset:
-        pass
+        # First, calculate the center of each box
+        centers = []
+        for box in data['boxes']:
+            centers.append([(box[0] + box[2]) / 2, (box[1] + box[3]) / 2])
+        # Second, calculate the distance between each center
+        dists = cdist(centers, centers)
+        # Third, find the k nearest neighbors
+        knn = NearestNeighbors(n_neighbors=k, metric='precomputed')
+        knn.fit(dists)
+        knn_rels.append(knn.kneighbors_graph().toarray())
+    # translate all these relations to direction
+    all_relation = []
+    for i, data in enumerate(dataset):
+        data_relations = []
+        for j, box in enumerate(data['boxes']):
+            for k, other_box in enumerate(data['boxes']):
+                if j == k:
+                    continue
+                relation = np.array([other_box[0] - box[0], other_box[1] - box[1]])
+                mag = np.linalg.norm(relation)
+                dir_normed = relation / np.linalg.norm(mag)
+                data_relations.append(dir_normed)
+        all_relation.append(data_relations)
+    # convert these relations to radians
+    all_relation = np.array(all_relation)
+    all_relation = np.arctan2(all_relation[:, :, 1], all_relation[:, :, 0])
+    # use kmeans to cluster these relations
+    # customized kmeans to use angle distance
+    kmeans = AngleKMeans(n_clusters=clusters, random_state=0).fit(all_relation.reshape(-1, 1))
+    # get the centers
+    centers = kmeans.centers_.reshape(-1)
+    # get the relations
+    final_rels = []
+    for i in range(len(centers)):
+        # calculate vector from angle
+        centers[i] = np.array([np.cos(centers[i]), np.sin(centers[i])])
+        final_rels.append(centers[i])
+    return final_rels
+
 
 def filter_relation(data_relation: List[BoxRel], thres=0.9):
     ''' If two relations are close enough in terms of cosine simiarlity, then we keep only the one that has smaller distance
@@ -41,7 +123,7 @@ def filter_relation(data_relation: List[BoxRel], thres=0.9):
         rm_idxs = []
         for i in range(len(rels)):
             for j in range(i + 1, len(rels)):
-                if np.dot(rels[i][3], rels[j][3]) > thres:
+                if np.dot(rels[i].projs, rels[j].projs) > thres:
                     rm_idxs.append(j)
         rels = [rels[i] for i in range(len(rels)) if i not in rm_idxs]
         filtered_data_relation.extend(rels)
@@ -92,4 +174,5 @@ def visualize_relation(dataset: Dataset, relation_set: List[Tuple[float, float]]
 if __name__ == '__main__':
     dataset = Dataset('data/train')
     relation_set = calculate_relation_set(dataset, 10, 4)
+    os.makedirs('data/vis', exist_ok=True)
     visualize_relation(dataset, relation_set, 'data/train_vis/')
