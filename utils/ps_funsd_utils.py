1fcac903557ae4f08da02138556fc77947035e47
datvo06 <datvo06@gmail.com>
2023-10-17 23:58:23 +1100

    Minor

--- utils/ps_funsd_utils.py
+++ utils/ps_funsd_utils.py
@@ -36,12 +36,15 @@ if __name__ == '__main__':
         out_bindings = batch_find_program_executor(nx_g, ps)
 
         uf = UnionFind(len(data['boxes']))
+        ucount = 0
         for j, p_bindings in enumerate(out_bindings):
             return_var = ps[j].return_variables[0]
             for w_binding, r_binding in p_bindings:
                 w0 = w_binding[WordVariable('w0')]
                 wlast = w_binding[return_var]
                 uf.union(w0, wlast)
+                ucount += 1
+        print(f"Union count: {ucount}")
 
         boxes = []
         words = []
