2f67edc315510fbc9e6fcd3121495f71d1d81e13
datvo06 <datvo06@gmail.com>
2023-10-20 22:41:42 +1100

    Minor fix

--- methods/decisiontree_ps_entity_linking_key_value.py
+++ methods/decisiontree_ps_entity_linking_key_value.py
@@ -91,6 +91,8 @@ def collect_program_execution_linking(programs, specs: SpecType, data_sample_set
                     tt[p].update([(i, w, w2) for w2 in w2otherwords[w] if w2 in w2e[w]])
                     tf[p].update([(i, w, w2) for w2 in w2otherwords[w] if w2 not in w2e[w]])
                     ft[p].update(w2e[w] - w2otherwords[w] - {w})
+    print("Total tt: ", sum(len(tt[p]) for p in programs))
+    input()
     return tt, ft, tf, all_out_mappings
 
 
@@ -335,7 +337,5 @@ if __name__ == '__main__':
         pos_paths = get_path_specs_linking(dataset, specs, relation_set=relation_set, data_sample_set_relation_cache=data_sample_set_relation_cache, cache_dir=args.cache_dir)
         with open(f"{args.cache_dir}/pos_paths_linking_kv.pkl", 'wb') as f:
             pkl.dump(pos_paths, f)
-    print(len(pos_paths))
-    input()
 
     programs = three_stages_bottom_up_version_space_based_entity_linking(pos_paths, dataset, specs, data_sample_set_relation_cache, args.cache_dir)
