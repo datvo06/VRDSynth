f16cac311743b84e673867a3971c2e9711f0e995
datvo06 <datvo06@gmail.com>
2023-10-21 16:26:57 +1100

    Minor fix

--- methods/decisiontree_ps.py
+++ methods/decisiontree_ps.py
@@ -348,13 +348,13 @@ def batch_find_program_executor(nx_g, find_programs: List[FindProgram]) -> List[
         path_to_programs[tuple(f.relation_constraint)].append((i, f))
 
     out_words = [[] for _ in range(len(find_programs))]
-    nx_g_removed_attrib = nx.MultiDiGraph()
+    nx_g_removed_attrib = nx.DiGraph()
     for u in nx_g.nodes():
         nx_g_removed_attrib.add_node(u)
     for u, v, k, d in nx_g.edges(data=True, keys=True):
-        nx_g_removed_attrib.add_edge(u, v, k)
+        nx_g_removed_attrib.add_edge(u, v)
     for path in path_to_programs:
-        nx_graph_query = nx.MultiDiGraph()
+        nx_graph_query = nx.DiGraph()
         word_vars = path_to_programs[path][0][1].word_variables
         for w in word_vars:
             nx_graph_query.add_node(w)
