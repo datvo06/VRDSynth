a3df99df886518abef0cf67749b03b98428d1fa2
datvo06 <datvo06@gmail.com>
2023-10-22 10:59:08 +1100

    Minor

--- models/layout_lmv3_utils.py
+++ models/layout_lmv3_utils.py
@@ -26,6 +26,7 @@ def get_word_embedding(data: DataSample):
     encoding = processor(image, data.words, boxes=list(normalize_bbox(b, width, height) for b in data.boxes), word_labels=[0]*len(data.boxes), 
                          return_tensors="pt")
     output = model(**encoding, output_hidden_states=True)
+    print(output)
     sequence_output = output.last_hidden_state[:, 1:(len(data.boxes)+1)]
     # sequence_output.shape = (1, 512, N)
     print(sequence_output)
