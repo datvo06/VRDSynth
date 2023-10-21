648bd6534c5eff86205216a5251531bc45b19c79
datvo06 <datvo06@gmail.com>
2023-10-22 10:58:01 +1100

    Minor

--- models/layout_lmv3_utils.py
+++ models/layout_lmv3_utils.py
@@ -1,4 +1,4 @@
-from transformers import AutoProcessor, AutoModelForTokenClassification
+from transformers import AutoProcessor, AutoModelForTokenClassification, AutoConfig
 from utils.funsd_utils import DataSample
 from PIL import Image
 import pickle as pkl
@@ -6,14 +6,24 @@ import pickle as pkl
 
 processor = AutoProcessor.from_pretrained("nielsr/layoutlmv3-finetuned-funsd",
                                           apply_ocr=False)
+config = AutoConfig.from_pretrained("nielsr/layoutlmv3-finetuned-funsd")
 model = AutoModelForTokenClassification.from_pretrained("nielsr/layoutlmv3-finetuned-funsd", num_labels=7)
 # These labels include [0, B-HEADER, I-HEADER, B-QUESTION, I-QUESTION, B-ANSWER, I-ANSWER]
+
+def normalize_bbox(bbox, width, height):
+     return [
+         int(1000 * (bbox[0] / width)),
+         int(1000 * (bbox[1] / height)),
+         int(1000 * (bbox[2] / width)),
+         int(1000 * (bbox[3] / height)),
+     ]
 
 def get_word_embedding(data: DataSample):
     # Load the image
     image = Image.open(
             data.img_fp.replace(".jpg", ".png")).convert("RGB")
-    encoding = processor(image, data.words, boxes=data.boxes, word_labels=[0]*len(data.boxes), 
+    width, height = image.size
+    encoding = processor(image, data.words, boxes=list(normalize_bbox(b, width, height) for b in data.boxes), word_labels=[0]*len(data.boxes), 
                          return_tensors="pt")
     output = model(**encoding, output_hidden_states=True)
     sequence_output = output.last_hidden_state[:, 1:(len(data.boxes)+1)]
