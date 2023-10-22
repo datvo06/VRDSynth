c3e3c68b6ab516d2bed5b684a59f93f3a22a8e8f
datvo06 <datvo06@gmail.com>
2023-10-22 22:06:08 +1100

    Update legacy graph utils

--- methods/decisiontree_ps.py
+++ methods/decisiontree_ps.py
@@ -381,9 +381,10 @@ def batch_find_program_executor(nx_g, find_programs: List[FindProgram]) -> List[
         for w1, w2, r in path:
             nx_graph_query.add_edge(w1, w2)
 
-        gm = isomorphism.DiGraphMatcher(nx_g, nx_graph_query)
-        print(len(list(gm.subgraph_isomorphisms_iter())))
+        print(nx_graph_query.nodes(), nx_graph_query.edges())
         input()
+
+        gm = isomorphism.DiGraphMatcher(nx_g, nx_graph_query)
         for subgraph in gm.subgraph_isomorphisms_iter():
             subgraph = {v: k for k, v in subgraph.items()}
             # get the corresponding binding for word_variables and relation_variables
