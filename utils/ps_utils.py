1a4840b332972969c21ea85a50f7506e7e7df445
datvo06 <datvo06@gmail.com>
2023-10-22 16:42:10 +1100

    Split words to chunk to avoid out of seq length

--- utils/ps_utils.py
+++ utils/ps_utils.py
@@ -1436,7 +1436,7 @@ def fill_hole(hole, max_depth=3, filter_strategy: FilterStrategy=None) -> list:
             except: # Invalid program
                 pass
     for i in range(len(all_programs)):
-        if all_programs[i].reduce()[0]:
+        if not isinstance(all_programs[i], list) and all_programs[i].reduce()[0]:
             all_programs[i] = all_programs[i].reduce()[1]
     if filter_strategy is not None and all_programs and not isinstance(all_programs[0], list):
         all_programs = [p for p in all_programs if filter_strategy.check_valid(p)]
