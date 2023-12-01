cc52db147220b6b32d39f7e9dab8f59f681a4907
datvo06 <datvo06@gmail.com>
2023-12-01 18:56:55 -0500

    Update run eval

--- run_eval.sh
+++ run_eval.sh
@@ -1,1 +1,1 @@
-lang=de;python -m utils.eval_linking --cache_dir_entity_linking cache_entity_linking_xfund_train_de_layoutlmv3_precision_counter_legacy_None_False_1.0/ --dataset xfund --lang de
+lang=$1;dataset=$2;python -m utils.eval_linking --cache_dir_entity_linking cache_entity_linking_${dataset}_train_${lang}_layoutlmv3_precision_counter_legacy_None_False_1.0/ --dataset ${dataset} --lang ${lang}
