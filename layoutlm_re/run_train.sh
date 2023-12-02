30e9875d7bd09a8d9c86f1e65c0cd78a3701c01e
datvo06 <datvo06@gmail.com>
2023-12-03 09:17:30 +1100

    Update run train

--- layoutlm_re/run_train.sh
+++ layoutlm_re/run_train.sh
@@ -1,1 +1,1 @@
-for lang in de es fr it ja pt zh; do python train.py ${lang} >> xfund_${lang}.log; done
+for lang in de es fr it ja pt zh; do python train.py ${lang} >> xfund_${lang}.log; sh ./cleanup_previous_checkpoint.sh; done
