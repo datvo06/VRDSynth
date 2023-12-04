20de73b5ebbc5be9bb570d6272edd18ad1de5211
datvo06 <datvo06@gmail.com>
2023-12-04 14:59:00 +1100

    Minor

--- layoutlm_re/inference.py
+++ layoutlm_re/inference.py
@@ -147,6 +147,9 @@ def infer(model, tokenizer_pre, tokenizer, collator, data_sample):
                     )
             for relation in outputs.pred_relations[0]:
                 hid, tid = relation['head_id'], relation['tail_id']
+                print(hid, tid, chunkg_entity)
+                if hid in chunk_entity and tid in chunk_entity:
+                    entities_map.append((hid, tid))
                 entities_map.append((chunk_entity[hid], chunk_entity[tid]))
     return entities_map
 
