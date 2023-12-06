67d145ccb7755076cfa9599fb9bd138b4e43ab8b
datvo06 <datvo06@gmail.com>
2023-12-06 14:45:00 +1100

    Update dataset

--- layoutlm_re/train.py
+++ layoutlm_re/train.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     if sys.argv[1] == 'en':
         dataset = load_dataset("./layoutlm_re/funsd")
     else:
-        dataset = load_dataset("R0bk/XFUN", f"xfun.{sys.argv[1]}")
+        dataset = load_dataset("./layoutlm_re/xfund", f"xfun.{sys.argv[1]}")
     train_dataset = dataset['train']
     test_dataset = dataset['validation']
 
