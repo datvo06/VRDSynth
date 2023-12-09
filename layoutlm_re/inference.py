80efe734f68b474e0963797064215fa208d8a68f
datvo06 <datvo06@gmail.com>
2023-12-09 15:21:37 +1100

    minor

--- layoutlm_re/inference.py
+++ layoutlm_re/inference.py
@@ -180,6 +180,7 @@ def convert_data_sample_to_input(data_sample, tokenizer):
 
 def prune_link_not_in_chunk(chunk_entities, relations):
     relation_full = set(tuple(t) for t in relations)
+    print(relation_full, chunk_entities)
     relation_spans = [[] for _ in range(len(chunk_entities))]
     for chunk_id, chunk_entities in enumerate(chunk_entities):
         relation_spans[chunk_id] = [(i, j) for (i, j) in relation_full if i in chunk_entities[chunk_id] and j in chunk_entities[chunk_id]]
