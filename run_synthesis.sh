1478afa29cfe44b3a91658b6aec508194c6b4661
datvo06 <hai@PizzaBox.PizzaBox>
2023-12-10 04:22:04 -0500

    Update script

--- run_synthesis.sh
+++ run_synthesis.sh
@@ -1,1 +1,1 @@
-dataset=$1;rel_type=legacy;thres=1.0;hops=3;for lang in $2;do python -m methods.decisiontree_ps_entity_linking --upper_float_thres 1.0 --rel_type=legacy --hops 3 --dataset ${dataset} --lang ${lang} --mode train --strategy=precision_counter;done
+dataset=$1;rel_type=legacy;thres=1.0;hops=$3;for lang in $2;do python -m methods.decisiontree_ps_entity_linking --upper_float_thres 1.0 --rel_type=legacy --hops ${hops:=3} --dataset ${dataset} --lang ${lang} --mode train --strategy=precision_counter;done
