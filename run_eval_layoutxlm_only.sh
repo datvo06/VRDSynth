2648d6065334c6d1da5418cb36e9d43075344bfd
datvo06 <datvo06@gmail.com>
2023-12-10 16:36:19 +1100

    minor

--- run_eval_layoutxlm_only.sh
+++ run_eval_layoutxlm_only.sh
@@ -1,1 +1,1 @@
-lang=$1;dataset=$2;python -m utils.eval_linking --cache_dir_entity_linking cache_entity_linking_${dataset}_train_${lang}_dummy_layoutxlm/ --lang ${lang} --use_layoutlm_output True --take_non_countered_layoutlm_output True
+lang=$1;dataset=xfund;if [ $1 ==  "en" ]; then dataset=funsd; fi; python -m utils.eval_linking --cache_dir_entity_linking cache_entity_linking_${dataset}_train_${lang}_dummy_layoutxlm/ --lang ${lang} --use_layoutlm_output True --take_non_countered_layoutlm_output True
