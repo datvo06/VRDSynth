830275b64f8d9ef8b9ae9159647cf7d3f20dbe42
datvo06 <datvo06@gmail.com>
2023-12-11 20:36:42 +1100

    Update chunking script

---
+++ run_eval_precision_chunking.sh
@@ -0,0 +1,1 @@
+lang=$1;dataset=xfund;if [ "$1" =  "en" ]; then dataset=funsd; fi; hops=$2;python -m utils.eval_linking --cache_dir_entity_linking cache_entity_linking_${dataset}_train_${lang}_${hops:=3}_layoutxlm_precision_legacy_None_False_1.0_False/ --dataset ${dataset} --lang ${lang} --eval_strategy 
