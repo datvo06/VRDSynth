969756e867053658b330bba4661e13057197b371
datvo06 <datvo06@gmail.com>
2023-12-11 19:12:35 +1100

    Remove redundant script

--- run_eval_layoutxlm_only_chunk_avg.sh
+++
@@ -1,1 +0,0 @@
-lang=$1;dataset=xfund;if [ "$1" =  "en" ]; then dataset=funsd; fi; python -m utils.eval_linking --cache_dir_entity_linking cache_entity_linking_${dataset}_train_${lang}_dummy_layoutxlm/ --lang ${lang} --use_layoutlm_output True --take_non_countered_layoutlm_output True --eval_strategy chunk_avg
