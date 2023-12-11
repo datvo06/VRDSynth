ee0215c3f180fa7645d5271bb1b75f186850ed31
datvo06 <datvo06@gmail.com>
2023-12-11 19:15:18 +1100

    Remove redundant script

--- run_eval_complement_layoutxlm.sh
+++ run_eval_complement_layoutxlm.sh
@@ -1,1 +1,1 @@
-lang=$1;dataset=$2;python -m utils.eval_linking --cache_dir_entity_linking cache_entity_linking_${dataset}_train_${lang}_layoutlmv3_precision_counter_legacy_None_False_1.0/ --dataset ${dataset} --lang ${lang} --use_layoutlm_output True
+lang=$1;dataset=$2;python -m utils.eval_linking --cache_dir_entity_linking cache_entity_linking_${dataset}_train_${lang}_layoutlmv3_precision_counter_legacy_None_False_1.0/ --dataset ${dataset} --lang ${lang} --use_layoutlm_output True --eval_strategy chunk
