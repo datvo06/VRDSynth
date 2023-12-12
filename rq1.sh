3ebc48cacaff290d645027c031f0286fd9d2ca5a
datvo06 <datvo06@gmail.com>
2023-12-12 22:38:19 +1100

    Update to include new data

--- rq1.sh
+++ rq1.sh
@@ -1,2 +1,3 @@
 for lang in en de es fr it ja pt zh; do (python -m layoutlm_re.eval ${lang} > rq1_layoutxlm_${lang}_chunk.txt & ); done
 for lang in en de es fr it ja pt zh; do (sh scripts/rq1/eval_chunking_full.sh ${lang} 3 > rq1_precision_counter_${lang}_chunk.txt & ); done
+for lang in en de es fr it ja pt zh; do (sh scripts/rq1/eval_chunking_kv.sh ${lang} 3 > rq1_precision_counter_${lang}_chunk_kv.txt & ); done
