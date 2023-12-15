207d77c8938e1546329a0d5124db8c6b3a2d4da6
datvo06 <datvo06@gmail.com>
2023-12-15 11:03:29 +1100

    Adding RQ1 and RQ2

--- scripts/miscs/run_synthesis.sh
+++ scripts/miscs/run_synthesis.sh
@@ -1,1 +1,1 @@
-dataset=$1;rel_type=legacy;thres=1.0;hops=$3;for lang in $2;do python -m methods.decisiontree_ps_entity_linking --upper_float_thres 1.0 --rel_type=legacy --hops ${hops:=3} --dataset ${dataset} --lang ${lang} --mode train --strategy=precision_counter;done
+rel_type=legacy;thres=1.0;hops=$2;for lang in $1;do python -m methods.decisiontree_ps_entity_linking --upper_float_thres 1.0 --rel_type=legacy --hops ${hops:=3} --lang ${lang} --mode train --strategy=precision_counter;done
