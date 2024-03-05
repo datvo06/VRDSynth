0dcb65dd1010921862df29b7be2f566f9cbfff83
datvo06 <datvo06@gmail.com>
2024-03-06 08:49:49 +1100

    [FEAT]: Adding infoxlm train

---
+++ infoxlm_re/train.py
@@ -0,0 +1,73 @@
+from transformers import AutoModel, AutoTokenizer
+from transformers import LayoutLMv2FeatureExtractor
+from .model import InfoXLMForRelationExtraction
+import sys
+from datasets import load_dataset
+from transformers import TrainingArguments
+from unilm.layoutlmft.layoutlmft.trainers import XfunReTrainer
+from layoutlm_re.train import compute_metrics, DataCollatorForKeyValueExtraction
+import argparse
+
+
+
+def get_model_and_tokenizer(args):
+    if args.model_type == 'infoxlm-base':
+        model = AutoModel.from_pretrained("microsoft/infoxlm-base")
+        tokenizer = AutoTokenizer.from_pretrained("microsoft/infoxlm-base")
+    elif args.model_type == 'infoxlm-large':
+        model = AutoModel.from_pretrained("microsoft/infoxlm-large")
+        tokenizer = AutoTokenizer.from_pretrained("microsoft/infoxlm-large")
+
+    model = InfoXLMForRelationExtraction(model)
+
+    return model, tokenizer
+
+def get_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_type', type=str, default='infoxlm-base')
+    parser.add_argument('--lang', type=str, default='en')
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_argparser()
+    args = parser.parse_args()
+    if args.lang == 'en':
+        dataset = load_dataset("./layoutlm_re/funsd")
+    else:
+        dataset = load_dataset("./layoutlm_re/xfund", f"xfun.{args.lang}")
+    train_dataset = dataset['train']
+    test_dataset = dataset['validation']
+
+    model, tokenizer = get_model_and_tokenizer(args)
+    feature_extractor = LayoutLMv2FeatureExtractor(apply_ocr=False)
+    training_args = TrainingArguments(output_dir=f"infoxlm-finetuned-xfund-{sys.argv[1]}-re",
+                                      overwrite_output_dir=True,
+                                      remove_unused_columns=False,
+                                      # fp16=True, -> led to a loss of 0
+                                      max_steps=40000,
+                                      save_total_limit=1,
+                                      per_device_train_batch_size=2,
+                                      per_device_eval_batch_size=2,
+                                      warmup_ratio=0.1,
+                                      learning_rate=1e-5,
+                                      )
+
+    data_collator = DataCollatorForKeyValueExtraction(
+        feature_extractor,
+        tokenizer,
+        pad_to_multiple_of=8,
+        padding="max_length",
+        max_length=512,
+    )
+    trainer = XfunReTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=test_dataset,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+    trainer.train()
+    print(trainer.evaluate())
