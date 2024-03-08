edc075f27e947be27ab7fea26cfea99722dcc4b3
datvo06 <datvo06@gmail.com>
2024-03-09 10:50:34 +1100

    [FEAT]: Adding README for table transformer

---
+++ rq4.sh
@@ -0,0 +1,13 @@
+######## DOWNLOAD MODELS ########
+# Table detection
+if [ ! -f pubtables1m_detection_detr_r18.pth ]; then
+	echo "Downloading pubtables1m_detection_detr_r18.pth"
+	wget https://huggingface.co/bsmock/tatr-pubtables1m-v1.0/resolve/main/pubtables1m_detection_detr_r18.pth
+fi
+# Table structure recognition
+if [ ! -f TATR-v1.1-All-msft.pth ]; then
+	echo "Downloading TATR-v1.1-All-msft.pth"
+	wget https://huggingface.co/bsmock/TATR-v1.1-All/resolve/main/TATR-v1.1-All-msft.pth
+fi
+
+######## Table model Infererence ########
