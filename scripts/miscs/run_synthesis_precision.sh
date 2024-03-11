b8db5e689383cb7f81e8de64a1f8fdeecbaed41f
datvo06 <datvo06@gmail.com>
2024-03-11 15:20:49 +1100

    Fix: Update argument for bash script

--- scripts/miscs/run_synthesis_precision.sh
+++ scripts/miscs/run_synthesis_precision.sh
@@ -1,1 +1,1 @@
-rel_type=legacy;thres=1.0;hops=3;for lang in $1;do python -m methods.decisiontree_ps_entity_linking --upper_float_thres 1.0 --rel_type=legacy --hops 3 --lang ${lang} --mode train --strategy=precision;done
+rel_type=legacy;thres=1.0;hops=3;for lang in $@;do python -m methods.decisiontree_ps_entity_linking --upper_float_thres 1.0 --rel_type=legacy --hops 3 --lang ${lang} --mode train --strategy=precision;done
--- scripts/miscs/run_synthesis_table.sh
+++ scripts/miscs/run_synthesis_table.sh
@@ -1,1 +1,1 @@
-rel_type=legacy;thres=1.0;hops=$2;for lang in $1;do python -m methods.decisiontree_ps_entity_linking --upper_float_thres 1.0 --rel_type=legacy_table --hops ${hops:=3} --lang ${lang} --mode train --strategy=precision_counter;done
+rel_type=legacy;thres=1.0;hops=3;for lang in $@;do python -m methods.decisiontree_ps_entity_linking --upper_float_thres 1.0 --rel_type=legacy_table --hops ${hops:=3} --lang ${lang} --mode train --strategy=precision_counter;done
