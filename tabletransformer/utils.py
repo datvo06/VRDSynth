a10f23f134683cb99814ffd7d37db3c33691ee47
datvo06 <datvo06@gmail.com>
2024-03-12 13:57:09 +1100

    [FEAT]: Update constraint filtering

--- tabletransformer/utils.py
+++ tabletransformer/utils.py
@@ -47,10 +47,16 @@ def build_nx_g_legacy_table_rc(entity_data: DataSample, table_out_dir: str):
     assert isinstance(tabs, list)
     ir2es = defaultdict(list)
     ic2es = defaultdict(list)
+    ir2bbox = {}
+    ic2bbox = {}
     for tidx, tab in enumerate(tabs):
         assert isinstance(tab, list)
         tab_rs = [o for o in tab if o["label"] == "table row"]
         tab_cs = [o for o in tab if o["label"] == "table column"]
+        for ridx, r in enumerate(tab_rs):
+            ir2bbox[(tidx, ridx)] = Bbox(r["x0"], r["y0"], r["x1"], r["y1"])
+        for cidx, c in enumerate(tab_cs):
+            ic2bbox[(tidx, cidx)] = Bbox(c["x0"], c["y0"], c["x1"], c["y1"])
         for eidx, bbox in enumerate(entity_data.boxes):
             ers = [i for i, o in enumerate(tab_rs) if iob(bbox, Bbox(*o["bbox"])) >= 0.3]
             ecs = [i for i, o in enumerate(tab_cs) if iob(bbox, Bbox(*o["bbox"])) >= 0.3]
@@ -63,19 +69,31 @@ def build_nx_g_legacy_table_rc(entity_data: DataSample, table_out_dir: str):
     # Same row would be index 5
     # Same column would be index 6
     edges = from_funsd_datasample(entity_data)
-    for (tidx, r), es in ir2es.items():
-        for i in range(len(es)):
-            for j in range(i+1, len(es)):
-                edges.append((es[i], es[j], 5))
-                edges.append((es[j], es[i], 5))
-    for (tidx, c), es in ic2es.items():
-        for i in range(len(es)):
-            for j in range(i+1, len(es)):
-                edges.append((es[i], es[j], 6))
-                edges.append((es[j], es[i], 6))
-
     data = entity_data
     nx_g = nx.MultiDiGraph()
+    for i, (box, label, word) in enumerate(zip(data.boxes, data.labels, data.words)):
+        if i not in nx_g.nodes():
+            nx_g.add_node(i)
+        nx_g.nodes[i].update({'x0': box[0], 'y0': box[1], 'x1': box[2], 'y1': box[3], 'label': label, 'word': word})
+
+    for (tidx, r), es in ir2es.items():
+        # Create a new node for the row
+        row_node = max(nx_g.nodes() if len(nx_g.nodes()) >= 0 else [-1]) + 1
+        nx_g.add_node(row_node)
+        nx_g.nodes[row_node].update({'label': "trow", 'x0': ir2bbox[(tidx, r)].x0, 'y0': ir2bbox[(tidx, r)].y0, 'x1': ir2bbox[(tidx, r)].x1, 'y1': ir2bbox[(tidx, r)].y1, 'word': ''})
+        for i in range(len(es)):
+            edges.append((row_node, es[i], 5))
+            edges.append((es[i], row_node, 5))
+    for (tidx, c), es in ic2es.items():
+        # Create a new node for the column
+        col_node = max(nx_g.nodes() if len(nx_g.nodes()) >= 0 else [-1]) + 1
+        nx_g.add_node(col_node)
+        nx_g.nodes[col_node].update({'label': "tcol", 'x0': ic2bbox[(tidx, c)].x0, 'y0': ic2bbox[(tidx, c)].y0, 'x1': ic2bbox[(tidx, c)].x1, 'y1': ic2bbox[(tidx, c)].y1, 'word': ''})
+
+        for i in range(len(es)):
+            edges.append((col_node, es[i], 6))
+            edges.append((es[i], col_node, 6))
+
     for i, j, etype in edges:
         # label is the index of max projection
         label = etype
@@ -85,10 +103,7 @@ def build_nx_g_legacy_table_rc(entity_data: DataSample, table_out_dir: str):
                     (data['boxes'][j][1] + data['boxes'][j][3]) / 2]
         mag = np.sqrt((center_i[0] - center_j[0]) ** 2 + (center_i[1] - center_j[1]) ** 2)
         nx_g.add_edge(i, j, mag=mag, lbl=label)
-    for i, (box, label, word) in enumerate(zip(data.boxes, data.labels, data.words)):
-        if i not in nx_g.nodes():
-            nx_g.add_node(i)
-        nx_g.nodes[i].update({'x0': box[0], 'y0': box[1], 'x1': box[2], 'y1': box[3], 'label': label, 'word': word})
+
     # Normalize the mag according to the smallest and largest mag
     mags = [e[2]['mag'] for e in nx_g.edges(data=True)]
     if len(mags) > 1:
