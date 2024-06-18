5c675d2d5abe1bf2a310d7b33d9e853ad3b668f6
datvo06 <datvo06@gmail.com>
2024-06-18 10:16:19 +1000

    Adding improved script

---
+++ scripts/rq1/eval_chun_full_improved.sh
@@ -0,0 +1,1 @@
+lang=$1;dataset=xfund;if [ "$1" =  "en" ]; then dataset=funsd; fi; hops=$2;python -m utils.eval_linking --cache_dir_entity_linking cache_entity_linking_improved_${dataset}_train_${lang}_${hops:=3}_layoutxlm_precision_counter_legacy_None_False_1.0_False/ --lang ${lang} --eval_strategy chunk
